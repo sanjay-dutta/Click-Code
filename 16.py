1732ffb6ea2dd48198eff7455215f290ea2e3784
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2024-10-24 14:27:15 +0100

    Updated D:\Github_Desktop\Click-Code\16.py

--- 16.py
+++ 16.py
@@ -1,17 +1,21 @@
-
 x = int(input("Enter the value of x: "))
-# x is the variable to match
+
 match x:
     # if x is 0
     case 0:
         print("x is zero")
-    # case with if-condition
+    # if x is 4
     case 4:
-        print("case is 4")
-
-    case _ if x!=90:
-        print(x, "is not 90")
-    case _ if x!=80:
-        print(x, "is not 80")
+        print("x is 4")
+    # if x is not 90
+    case _ if x != 90 and x != 80:
+        print(x, "is neither 90 nor 80")
+    # if x is not 90
+    case _ if x == 90:
+        print(x, "is 90")
+    # if x is not 80
+    case _ if x == 80:
+        print(x, "is 80")
+    # default case
     case _:
         print(x)
