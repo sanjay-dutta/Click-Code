fe1b07b15ec52249dcbefe7a7633b2f889d534f6
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2024-10-24 14:36:08 +0100

    Updated D:\Github_Desktop\Click-Code\16.py

--- 16.py
+++ 16.py
@@ -7,15 +7,15 @@ match x:
     # if x is 4
     case 4:
         print("x is 4")
-    # if x is not 90
-    case _ if x != 90 and x != 80:
-        print(x, "is neither 90 nor 80")
-    # if x is not 90
+    # if x is 90
     case _ if x == 90:
         print(x, "is 90")
-    # if x is not 80
+    # if x is 80
     case _ if x == 80:
         print(x, "is 80")
-    # default case
-    case _:
-        print(x)
+    # if x is 100
+    case _ if x == 100:
+        print(x, "is 100")
+    # if x is neither 90 nor 80
+    case _ if x != 90 and x != 80:
+        print(x, "is neither 90 nor 80")
