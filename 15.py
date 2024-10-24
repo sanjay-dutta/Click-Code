1f13929c6a8eb5cc8146af679e0fcc46b81873f5
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2024-10-24 13:28:41 +0100

    Updated D:\Github_Desktop\Click-Code\15.py

--- 15.py
+++ 15.py
@@ -7,6 +7,6 @@ timestamp = time.strftime('%M')
 print(timestamp)
 timestamp = time.strftime('%S')
 print(timestamp)
-# https://docs.python.org/3/library/time.html#time.strftime
+print("Hi Sanjay, how are you?")
 
 
